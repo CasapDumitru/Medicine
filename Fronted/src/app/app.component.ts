d8a6980a24b75c1a9f23d6a77066648f69dfd57a
Casap Dumitru <casapdumitru@yahoo.com>
2018-12-17 22:33:00 +0200

    Fronted Setup

---
+++ Fronted/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "medicine-root",
+  templateUrl: "./app.component.html"
+})
+
+export class AppComponent {
+  pageTitle: string = "Medicine Application";
+}
